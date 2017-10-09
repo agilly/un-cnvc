6062e1b36c7f506ca91bd311c454bde8951d4247
Arthur Gilly <agilly@users.noreply.github.com>
2017-10-09 16:07:34 +0100

    Update searchsv.sh

--- searchsv.sh
+++ searchsv.sh
@@ -2,7 +2,16 @@
 chr=$1
 start=$2
 end=$3
-prefix="/lustre/scratch115/realdata/mdt0/projects/t144_helic_15x/analysis/HA/release/chr"
+
+#########################################
+## Warning: This script uses 2 threads ##
+#########################################
+
+## Edit below to generate the path for your VCFs
+prefix=""
 suffix=""
+
+## By default, runs 10Mb chunks. Reduce if your machine is low in RAM or has a lousy processor.
+
 bcftools view -r 'chr'$chr':'${start}'0000000-'${end}'0000000' ${prefix}${chr}${suffix}.vcf.gz  | bcftools query -f '%CHROM\t%POS\t[%DP\t]\n' > $chr.${start}-${end}M.alldp
 ./call_sv.R $chr.${start}-${end}M.alldp chr$chr.avg.depth.persample $chr.${start}-${end}M.sv
